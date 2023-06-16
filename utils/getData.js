5f9393cb1a1c7e6b03cc1ee7a8cc548b4aa8598c
david <davidLiao@hahow.in>
2023-06-16 16:38:45 +0800

    fix: update getData href

--- utils/getData.js
+++ utils/getData.js
@@ -1,6 +1,9 @@
 function getData(functionName) {
   const baseUrl = window.location.origin + window.location.pathname;
-  return fetch(
-    `${baseUrl}benchmarks/${functionName}/${functionName}.json`
-  ).then((res) => res.json());
+  const href = new URL(
+    `benchmarks/${functionName}/${functionName}.json`,
+    baseUrl
+  ).href;
+
+  return fetch(href).then((res) => res.json());
 }
